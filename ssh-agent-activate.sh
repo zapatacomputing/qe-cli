fea69baefc989f77079676ba22de0d0951839315
Steve Chin <steve.chin@zapatacomputing.com>
2020-09-03 18:02:12 -0400

    [#174626153] adding ssh-agent-activate.sh
    Signed-off-by: BK Lau <bk.lau@zapatacomputing.com>

---
+++ ssh-agent-activate.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Purpose of this script:
+# Allows re-use of the ssh-agent between different shell sessions/
+
+# This script should be appended to your .bashrc; 
+
+# Append the following:
+# source ~/scripts/ssh-agent-activate.sh
+
+# The logic of this script is as follows:
+# 1. try to execute ssh-add -l, and redirect output to the /dev/null
+# 2. check returned code of the previous command:
+#   if it is == 2 (error connect to an agent):
+#     check if ~/.ssh-agent-env is present and available for reading,  read it and pass its output to the bash
+#     retry ssh-add -l
+#     if code still 2:
+#       create the ~/.ssh-agent-env file with the 660 permissions (read-write for an owner only)
+#       start ssh-agent and redirects its output into the .ssh-agent-env file
+#       read the .ssh-agent-env content and pass it via a pipe to the bash
+
+ssh-add -l &>/dev/null
+if [ "$?" == 2 ]; then
+  test -r ~/.ssh-agent-env && \
+    eval "$(<~/.ssh-agent-env)" >/dev/null
+  ssh-add -l &>/dev/null
+  if [ "$?" == 2 ]; then
+    (umask 066; ssh-agent > ~/.ssh-agent-env)
+    eval "$(<~/.ssh-agent-env)" >/dev/null
+  fi
+fi
